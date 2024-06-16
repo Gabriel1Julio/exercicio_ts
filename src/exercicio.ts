f1b9653c59925d70d836af4212100d4493de46bc
Gabriel Julio ALves de Souza <gj86225@gmail.com>
2024-06-16 14:59:13 -0300

    primeiro commit

---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicarValores(valorX: number, valorY: number):number {
+    return valorX * valorY
+}
+
+console.log(multiplicarValores(7, 9));
+
+function dizOla(nome: string):string {
+    return `Olá ${nome}`
+}
+
+console.log(dizOla('Gabriel'));
